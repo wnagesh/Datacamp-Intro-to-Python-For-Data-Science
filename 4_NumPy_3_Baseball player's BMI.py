9b2df41af5b533e7b94d27f4b807ca2f3ae76215
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2019-01-12 03:18:01 +0600

    Update 4_NumPy_3_Baseball player's BMI.py

--- 4_NumPy_3_Baseball player's BMI.py
+++ 4_NumPy_3_Baseball player's BMI.py
@@ -4,10 +4,10 @@
 import numpy as np
 
 # Create array from height with correct units: np_height_m
-np_height_m = np.array(height) * 0.0254
+np_height_m = np.array(height_in) * 0.0254
 
 # Create array from weight with correct units: np_weight_kg
-np_weight_kg=np.array(weight)*0.453592
+np_weight_kg=np.array(weight_lb)*0.453592
 
 # Calculate the BMI: bmi
 bmi=np_weight_kg/(np_height_m **2)
