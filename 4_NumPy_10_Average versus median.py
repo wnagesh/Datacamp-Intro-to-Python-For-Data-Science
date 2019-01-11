d6a3e015250e1169ba5ac02129f9c2ac619f5b72
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2019-01-12 03:50:50 +0600

    Update 4_NumPy_10_Average versus median.py

--- 4_NumPy_10_Average versus median.py
+++ 4_NumPy_10_Average versus median.py
@@ -4,10 +4,10 @@
 import numpy as np
 
 # Create np_height from np_baseball
-np_height=np_baseball[:,0]
+np_height_in=np_baseball[:,0]
 
 # Print out the mean of np_height
-print(np.mean(np_height))
+print(np.mean(np_height_in))
 
 # Print out the median of np_height
-print(np.median(np_height))
+print(np.median(np_height_in))
